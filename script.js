69e7d87121bb8feef65c3603869405c747d9da41
unknown <ppalesa85@gmail.com>
2021-11-08 16:11:03 +0200

    names added

---
+++ script.js
@@ -0,0 +1,21 @@
+const names =["Kagisho","Andisiwe","Mmapaseka","Wandile","Slina","Mdudzi","Sophie","Lucky","Happy","precious","Mhlompeni","Nkosiphendule","vusumuzi"];
+
+
+const display=document.getElementById("display");
+for(let i=0; i < names.length; i++){
+    display.innerHTML += "<li>" + names[i] + "</li>";
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
