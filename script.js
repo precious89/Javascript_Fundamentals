c87d885f112cdcd61047c1941f93981d2836b31f
unknown <ppalesa85@gmail.com>
2021-11-09 14:25:47 +0200

    underline added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const names =["Kagisho","Andisiwe","Mmapaseka","Wandile","Slina","Mdudzi","Sophie","Lucky","Happy","precious","Mhlompeni","Nkosiphendule","vusumuzi"];
+const names =["Kagisho","Andisiwe","Mmapaseka","Wandile","Slina","Mdudzi","Sophie","Lucky","Happy","precious","Mhlompeni","Nkosiphendule","vusumuzi","hardy"];
 
 
 const display=document.getElementById("display");
 for(let i=0; i < names.length; i++){
-    display.innerHTML += "<li>" + names[i] + "</li>";
+    display.innerHTML += "<hr>" + names[i] + "</hr>";
 }
 
 
